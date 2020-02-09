f51ac63665692397d0fbd2543e49aa374599b9b0
laufgas <laufgas@gmail.com>
2020-02-09 21:41:47 +0200

    Updates

---
+++ precache-manifest.d2bb0814e2176c0eee2260166531565f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675a2dbd29d8a6c7899f28c00cb3ede3",
+    "url": "/robotcardsapp/index.html"
+  },
+  {
+    "revision": "d71238a09fab2102aadf",
+    "url": "/robotcardsapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8022368a3b5c5140e0dc",
+    "url": "/robotcardsapp/static/css/main.3a0c0d94.chunk.css"
+  },
+  {
+    "revision": "d71238a09fab2102aadf",
+    "url": "/robotcardsapp/static/js/2.c74feeab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotcardsapp/static/js/2.c74feeab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8022368a3b5c5140e0dc",
+    "url": "/robotcardsapp/static/js/main.5455e58a.chunk.js"
+  },
+  {
+    "revision": "d4b59af36c48d33b27e6",
+    "url": "/robotcardsapp/static/js/runtime-main.946aece4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotcardsapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
