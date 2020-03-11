51747015830ba5168f4392b6247ab2bf7188388e
laufgas <laufgas@gmail.com>
2020-03-11 12:29:36 +0200

    fixed a typo

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,8 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => {{{this.setState( { robots: users })}}});
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
   }
 
   onSearchChange = (event) => {
