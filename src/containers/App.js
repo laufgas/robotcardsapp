339168cfa2a858ca1993a3b9e9c562a79b2fa4d1
laufgas <laufgas@gmail.com>
2020-03-11 13:49:22 +0200

    fix 5

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users' + 'https://cors-anywhere.herokuapp.com/')
       .then(response=> response.json())
       .then(users => {this.setState({ robots: users})});
   }
